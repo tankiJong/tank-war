d3f46f4f69f149d14c5e0b7eb56d7675b3a098f1
Tanki Zhang <tanki_jong@hotmail.com>
2018-06-02 15:39:53 -0500

    clean up code

--- Code/Game/Game.hpp
+++ Code/Game/Game.hpp
@@ -1,9 +1,6 @@
 #pragma once
 #include "Engine/Renderer/Camera.hpp"
-#include "Engine/Framework/Light.hpp"
 #include "Engine/Renderer/RenderScene.hpp"
-#include "Engine/Renderer/Shader/Material.hpp"
-#include "Engine/Renderer/Renderable/Renderable.hpp"
 #include "Engine/Effect/ParticleEmitter.hpp"
 class SpriteSheet;
 class Clock;
@@ -22,21 +19,12 @@ public:
   void beforeFrame();
   void afterFrame();
 
-  void addLight();
 protected:
 
   void updateLight();
   Camera * mCamera;
   Clock* mGameClock;
-  S<const Mesh> mShip = nullptr;
-  Mesh* mSphere = nullptr;
-  const Material* mCoachMat;
-  const Material* mShipMat;
-  ParticleEmitter* mEmitter;
-  Light mLight1, mLight2, mLight3;
   RenderScene* mRenderScene;
-  Renderable mRship;
-  Renderable mRsphere;
 };
 
 extern Game* g_theGame;
